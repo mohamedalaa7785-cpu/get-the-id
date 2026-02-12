04d42bcf8846d4e37d9ff643c05bd336742db832
Muhammed Alaa <mohamed.alaa7785@gmail.com>
2026-02-12 09:20:01 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{textAlign:"center", marginTop:"100px", fontFamily:"Arial"}}>
+      <h1>Get The ID</h1>
+      <p>الموقع شغال 🎉</p>
+    </div>
+  );
+    }
